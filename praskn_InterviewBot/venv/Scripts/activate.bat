d5407084fcb8f795cd5b16fc7e1ca771880b86ab
Manish <manishgodbole022@gmail.com>
2024-04-08 21:34:02 +0530

    Videobot committed

--- praskn_InterviewBot/venv/Scripts/activate.bat
+++ praskn_InterviewBot/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\USER\OneDrive\Desktop\VirJ\venv
+set VIRTUAL_ENV=C:\Users\USER\OneDrive\Desktop\Impetus\praskn_InterviewBot\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
